239629fc5529a457e3272af4a09cadd04d03921f
Yash <yash8135512@gmail.com>
2022-10-06 01:58:18 +0530

    Make pre-populate category slug

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Cateogory
 
 
-admin.site.register(Cateogory)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug')
+
+
+admin.site.register(Cateogory, CategoryAdmin)
