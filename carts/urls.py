6b0b9999b95229f9b071ae37a00eb3612777653c
Yash <yash8135512@gmail.com>
2022-10-08 02:00:50 +0530

    Add to cart functionality without logging-in and with session key

--- carts/urls.py
+++ carts/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart')
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from carts.models import CartItem
+from store.models import Product
+from .models import Cart,CartItem
+
+from django.http import HttpResponse 
+
+
+def _cart_id(request):
+	# fetch session_key from request and create session_key if it is null
+	cart = request.session.session_key
+	if not cart:
+		cart = request.session.create()
+	return cart
+
+
+def add_cart(request, product_id):
+	# get the product
+	product = Product.objects.get(id = product_id)  
+	try:
+		""" 
+		# get the cart using cart_id present in the session based on current request,
+		# so that it will match cart_id with session_id
+
+		"""
+		cart = Cart.objects.get(cart_id = _cart_id(request))
+	except Cart.DoesNotExist:
+		cart = Cart.objects.create(cart_id = _cart_id(request))
+		cart.save()
+
+	try:
+		# to put fetched product inside cart,eventuall all fetched products will become cart_item
+		# here combining product and cart to make cart_item
+		cart_item = CartItem.objects.get(product = product, cart = cart)
+		cart_item.quantity += 1
+		cart_item.save()
+
+	except CartItem.DoesNotExist:
+		cart_item = CartItem.objects.create(
+			product = product,
+			cart = cart,
+			quantity = 1,
+		)
+		cart_item.save()
+
+	return HttpResponse(cart_item.quantity)
+	exit()
+	return redirect('cart')
 
 def cart(request):
-    return render(request, 'store/cart.html')
+	""" to render cart item and its content """
+	return render(request, 'store/cart.html')
